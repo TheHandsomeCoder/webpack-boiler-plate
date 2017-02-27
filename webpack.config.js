404f30f2ecf06c5bfe20b09b6aeeed548976ebdd
Scott O'Malley <scottyomalley@gmail.com>
2017-02-27 16:36:09 +0000

    Update webpack.config.js
    Updated Babel

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
             exclude: /node_modules/,
             loader: 'babel-loader',
             query:{
-                presets: ['es2015-native-modules']
+                presets: ['es2015', {'modules': false}]
             }
         }]
     },
